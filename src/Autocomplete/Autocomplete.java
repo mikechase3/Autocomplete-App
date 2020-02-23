9f9ae07bb34619e497102f2c11194617708ea133
dai01-cps <61386373+dai01-cps@users.noreply.github.com>
2020-02-23 17:55:47 -0500

    allMatches(String prefix)
    I think Josha should read this part as I've used your byPrefixOrder(length), I'm not sure whether it's correct.

--- src/Autocomplete/Autocomplete.java
+++ src/Autocomplete/Autocomplete.java
@@ -1,5 +1,22 @@
 package Autocomplete;
 
+import java.util.Comparator;
+
+/**In this part, you will implement a data type that provides autocomplete functionality
+for a given set of string and weights, 
+using Term and BinarySearchDeluxe. 
+To do so, sort the terms in lexicographic order; 
+use binary search to find the all query strings 
+that start with a given prefix; and sort the matching terms in descending order by weight. 
+constructor:
+	merge sort + byPrefixOrder(int r)
+allMatches:
+	BinarySearchDeluxe->first & last;
+	sort(Terms,first,last,byReverseWeightOrder());
+	Mterms = Terms[first:last];
+	return Mterms;
+*/
+//where to use the byPrefixOrder(int r)
 public class Autocomplete {
 
     //Member Variables
@@ -12,7 +29,8 @@ public class Autocomplete {
      */
 	public Autocomplete(Term[] terms)
 	{
-		this.terms=terms;
+		int length = terms.length;
+		mergesort(terms,terms.begin,terms.end,byPrefixOrder(length));
 	}
 
 	//Member Methods
@@ -24,7 +42,28 @@ public class Autocomplete {
     public Term[] allMatches(String prefix){
     	return terms;
     }
-
+    
+    private void mergesort(Term[] terms,int begin,int end, Comparator camp)
+    {
+    	if(begin>end) return;
+    	int mid = begin+(end-begin)/2;
+    	mergesort(terms,begin,mid,camp);
+    	mergesort(terms,mid+1,end,camp);
+    	merge(terms,begin,mid,end,camp);
+    }
+    private void merge(Term[] terms,int begin,int mid,int end,Comparator<T> camp)
+    {
+    	Term[] aux;
+    	for(int i = begin;i<=end;i++) aux[i] = terms[i];    
+    	int i = begin,j = mid,k = begin;
+    	while(i<=mid&&j<=end&&k<end)
+    	{
+    		if(camp.compare(a[i], a[j])>0) terms[k++] = aux[j++];//a[i]>a[j],aux[k] = a[j]
+    		else terms[k++] = aux[i++];
+    	}
+    	while(i<=mid) terms[k++] = aux[i++];
+    	//j part has already there
+    }
 }
 
 //>>>>>>> 63e9b4bd3fbde97c6d41eabf20ce2386a5671654
