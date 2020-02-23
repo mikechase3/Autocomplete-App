ceaab9c2dafe2a5eea033ad78104965c0b9eec56
Michael Chase <me@mchase.me>
2020-02-23 13:50:03 -0500

    Started the binary search file

--- src/Autocomplete/Main.java
+++ src/Autocomplete/Main.java
@@ -1,10 +1,15 @@
 package Autocomplete;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hi");
+
+        Autocomplete autocompleteObject = new Autocomplete();
+        ArrayList<Integer> something = new ArrayList<Integer>();
     }
 
 }
