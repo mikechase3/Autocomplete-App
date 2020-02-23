1ff4949331ecb6e8a0ad2f5fe2ed5af2a151708e
Michael Chase <me@mchase.me>
2020-02-23 13:37:52 -0500

    Started the binary search file

--- src/Autocomplete/Autocomplete.java
+++ src/Autocomplete/Autocomplete.java
@@ -1,18 +1,30 @@
 package Autocomplete;
 
 public class Autocomplete {
-<<<<<<< HEAD
+
+    //Member Variables
 	Term[] terms;
+
+	//Constructor
+    /**
+     * Initializes the data structure from the given array of terms
+     * @param terms
+     */
 	public Autocomplete(Term[] terms)
 	{
 		this.terms=terms;
 	}
-}	
-=======
-    //Member Variables
 
-    //Constructor
+	//Member Methods
+    /**
+     * Returns all terms that start with the given prefix, in descending order of weight
+     * @param prefix
+     * @return
+     */
+    public Term[] allMatches(String prefix){
 
-    //Member Methods
+    }
+
 }
->>>>>>> 63e9b4bd3fbde97c6d41eabf20ce2386a5671654
+
+//>>>>>>> 63e9b4bd3fbde97c6d41eabf20ce2386a5671654
