db2bb6c49be56c25435fae1190f4a56aa3c26308
dai01-cps <61386373+dai01-cps@users.noreply.github.com>
2020-03-03 12:15:36 -0500

    my FILE
    .

--- src/Autocomplete/Autocomplete.java
+++ src/Autocomplete/Autocomplete.java
@@ -1,6 +1,10 @@
 package Autocomplete;
 
+import java.rmi.NoSuchObjectException;
+import java.util.*;  
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Collections;
 
 /**In this part, you will implement a data type that provides autocomplete functionality
 for a given set of string and weights, 
@@ -20,8 +24,7 @@ allMatches:
 public class Autocomplete {
 
     //Member Variables
-	Term[] terms = new Term[];
-
+	Term[] terms;
 	//Constructor
     /**
      * Initializes the data structure from the given array of terms
@@ -29,10 +32,14 @@ public class Autocomplete {
      */
 	public Autocomplete(Term[] terms)
 	{	//check if any of the entries in its argument array are null.
-		if(!terms) throw java.lang.NullPointerException;
 		this.terms = terms;
-		int length = terms.length()
-		mergesort(terms,0,,length-1,Term.byReverseWeightOrder());
+		if(terms == null) throw new java.lang.NullPointerException();
+		this.terms = terms;
+		int length = terms.length;
+	//	mergesort(terms,0,length-1,Term.byPrefixOrder(length)); have to be this version,but something goes wrong with the comp.
+		mergesort(terms,0,length-1,Term.byReverseWeightOrder());
+	//	Array.ssort(terms,0,length-1);
+	//	Collections.sort(terms, Term.byPrefixOrder(length));
 	}
 
 	//Member Methods
@@ -40,42 +47,45 @@ public class Autocomplete {
      * Returns all terms that start with the given prefix, in descending order of weight
      * @param prefix
      * @return
+     * @throws NoSuchObjectException 
      */
 	/*public Term[] allMatches(String prefix)
 {
 Comparator comp = Term.byPrefixOrder(prefix.length());
 Term key = new Term(prefix, ??);
 int begin = BinarySearchDeluxe.firstIndexOf(terms, key, comp);
-if (¡­) return ¡­; // no matches
+if (..) return ..; // no matches
 int end = ¡­;
 Term[] match = new Term[end ¨C begin + 1];
 ¡­ // merge sort array match byReverseWeightOrder();
 return match;
 }*/
 	// I guess Key is a type that hasn't specified.
-    public Term[] allMatches(String prefix){
-    	if(!prefix) throw java.lang.NullPointerException;
-    	 Term term = new Term(prefix,(lnog)0);
-    	 Term[] NoMathch = new Term[1];
-    	 if(!trem) throw java.lang.NullPointerException;
-    	 if(!NoMatch) throw java.lang.NullPointerException;
-    	 NoMathch[0].query = "No match";
+    public Term[] allMatches(String prefix) throws NoSuchObjectException{
+    	if(prefix == null) throw new java.lang.NullPointerException();
+    	 Term term = new Term(prefix,(long)0);
+    	 Term[] NoMatch = new Term[1];
+    	 Term M = new Term("no match",0);
+    	 NoMatch[0] = M;
+    	 if(term == null) throw  new java.lang.NullPointerException();
+    	 if(NoMatch == null) throw  new java.lang.NullPointerException();
+    	 NoMatch[0].query = "No match";
     	 term.query = prefix;
-    	 int length = prefix.length()£»
+    	 int length = prefix.length();
     	 int first = BinarySearchDeluxe.firstIndexOf(terms, term, Term.byPrefixOrder(length));
     	 //???
-    	 if(first == -1) return NoMathch;
+    	 if(first == -1) return NoMatch;
     	 int last = BinarySearchDeluxe.lastIndexOf(terms, term, Term.byPrefixOrder(length));
     	 mergesort(terms,first,last,Term.byReverseWeightOrder());
     	 Term[] Mterms = new Term[last-first+1];
-    	 for(int i = first;i<last;i++) Mterms[i] = terms[i];
-    	 mergesort(Mterms,first,last,Term,byReverseWeightOrder());
+    	 for(int i = first;i<=last;i++) Mterms[i-first] = terms[i];
+    	 mergesort(Mterms,0,Mterms.length-1,Term.byReverseWeightOrder());
     	return Mterms;
     }
     // I guess Key is a type that hasn't specified.
     private void mergesort(Term[] terms,int begin,int end, Comparator camp)
     {
-    	if(begin>end) return;
+    	if(begin>=end) return;
     	int mid = begin+(end-begin)/2;
     	mergesort(terms,begin,mid,camp);
     	mergesort(terms,mid+1,end,camp);
@@ -85,8 +95,8 @@ return match;
     {
     	Term[] aux = new Term[terms.length];
     	for(int i = begin;i<=end;i++) aux[i] = terms[i];    
-    	int i = begin,j = mid,k = begin;
-    	while(i<=mid&&j<=end&&k<end)
+    	int i = begin,j = mid+1,k = begin;// It should be j = mid+1, otherwise out of range.
+    	while(i<=mid&&j<=end&&k<=end)
     	{
     		if(camp.compare(terms[i], terms[j])>0) terms[k++] = aux[j++];//a[i]>a[j],aux[k] = a[j]
     		else terms[k++] = aux[i++];
