df789da792c736bfaa5de436636dc26f04447fb1
cowinaden <39162410+cowinaden@users.noreply.github.com>
2020-03-07 14:18:33 -0500

    Update Autocomplete.java

--- src/Autocomplete/Autocomplete.java
+++ src/Autocomplete/Autocomplete.java
@@ -37,10 +37,9 @@ public class Autocomplete {
 		this.terms = terms;
 		int length = terms.length;
    	 //Arrays.parallelSort(terms, 0, length-1, Term.byPrefixOrder(length));
-		//mergesort(terms,0,length-1,Term.byPrefixOrder(1)); //have to be this version,but something goes wrong with the comp.
-	//	mergesort(terms,0,length-1,Term.byReverseWeightOrder());
-	//	Array.ssort(terms,0,length-1);
-	//	Collections.sort(terms, Term.byPrefixOrder(length));
+   	 mergesort(terms,0,length-1,Term.byPrefixOrder(length)); 
+   	 //have to be this version,but something goes wrong with the comp.
+
 	}
 
 	//Member Methods
@@ -63,55 +62,57 @@ return match;
 }*/
 	// I guess Key is a type that hasn't specified.
     public Term[] allMatches(String prefix) throws NoSuchObjectException{
+    	//Confirming correct input
     	if(prefix == null) throw new java.lang.NullPointerException();
-		Arrays.parallelSort(terms, 0, terms.length-1, Term.byPrefixOrder(prefix.length()));
+		// Set-Up Local Variables
     	 Term term = new Term(prefix,(long)0);
     	 Term[] NoMatch = new Term[1];
     	 Term M = new Term("no match",0);
     	 NoMatch[0] = M;
+    	 // Confirm required inputs for local variables
     	 if(term == null) throw  new java.lang.NullPointerException();
     	 if(NoMatch == null) throw  new java.lang.NullPointerException();
     	 NoMatch[0].query = "No match";
     	 term.query = prefix;
     	 int length = prefix.length();
+    	 // Find first instance of the prefix in the given file
     	 int first = BinarySearchDeluxe.firstIndexOf(terms, term, Term.byPrefixOrder(length));
-    	 //???
-    	 System.out.println("first: "+first);
+    	 // Test code
+    	 //System.out.println("first: "+first);
+    	 // Check to see if it should end early
     	 if(first == -1) return NoMatch;
+    	 // Find the last index of the prefix in the given file
     	 int last = BinarySearchDeluxe.lastIndexOf(terms, term, Term.byPrefixOrder(length));
-    	 //system.out
-    	 //Arrays.sort(terms);
-    	 //Arrays.parallelSort(terms, first, last, Term.byReverseWeightOrder());
-    	 mergesort(terms,first,last,Term.byReverseWeightOrder());
+    	 // Creates the array to return with required space
     	 Term[] Mterms = new Term[last-first+1];
+    	 //It's the matched terms:copies the terms from the file array to the return array
     	 for(int i = first;i<=last;i++) Mterms[i-first] = terms[i];
     	 //Arrays.sort(Mterms);
     	 //Arrays.parallelSort(Mterms, 0, Mterms.length-1, Term.byReverseWeightOrder());
+    	 //sorts the return array in reverse weight order 
     	mergesort(Mterms,0,Mterms.length-1,Term.byReverseWeightOrder());
     	return Mterms;
     }
     // I guess Key is a type that hasn't specified.
-    private void mergesort(Term[] terms,int begin,int end, Comparator camp)
+    private void mergesort(Term[] terms,int begin,int end, Comparator comp)
     {
     	if(begin>=end) return;
     	int mid = begin+(end-begin)/2;
-    	mergesort(terms,begin,mid,camp);
-    	mergesort(terms,mid+1,end,camp);
-    	merge(terms,begin,mid,end,camp);
+    	mergesort(terms,begin,mid,comp); //Merge sort with the first half
+    	mergesort(terms,mid+1,end,comp); //Merge sort, break down second half
+    	merge(terms,begin,mid,end,comp); //Merges the two halves together. 
     }
-    private void merge(Term[] terms,int begin,int mid,int end,Comparator<Term> camp)
+    private void merge(Term[] terms,int begin,int mid,int end,Comparator<Term> comp)
     {
     	Term[] aux = new Term[terms.length];
     	for(int i = begin;i<=end;i++) aux[i] = terms[i];    
     	int i = begin,j = mid+1,k = begin;// It should be j = mid+1, otherwise out of range.
     	while(i<=mid&&j<=end&&k<=end)
     	{
-    		if(camp.compare(terms[i], terms[j])>0) terms[k++] = aux[j++];//a[i]>a[j],aux[k] = a[j]
+    		if(comp.compare(terms[i], terms[j])>0) terms[k++] = aux[j++];//a[i]>a[j],aux[k] = a[j]
     		else terms[k++] = aux[i++];
     	}
     	while(i<=mid) terms[k++] = aux[i++];
     	//j part has already there
     }
 }
-
-//>>>>>>> 63e9b4bd3fbde97c6d41eabf20ce2386a5671654
